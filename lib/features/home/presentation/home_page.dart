ba4ccd6475522c8f3f0f4d9ce4d2851734d7aa3f
eclipse021 <yahang2008@naver.com>
2025-12-01 06:46:07 +0900

    feat: 홈 화면에 main_banner 구현 및 적용

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ssogssog_flutter/features/home/presentation/widget/home_app_bar.dart';
+import 'package:ssogssog_flutter/features/home/presentation/widget/main_banner.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,10 +10,9 @@ class HomePage extends StatelessWidget {
     return const Scaffold(
       appBar: HomeAppBar(),
       body: SingleChildScrollView(
-        // SingleChildScrollView를 사용해 화면이 길어져도 스크롤 가능하게 함
         child: Column(
           children: [
-            // 2. 메인 배너 (곧 만들 예정)
+            MainBanner(), // 메인 배너 추가
             // 3. 액션 블록 (곧 만들 예정)
             // 4. 내가 찜한 주식 배너 (곧 만들 예정)
             // 5. 팩트 체크 섹션 (곧 만들 예정)
