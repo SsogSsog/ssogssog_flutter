bac907ad9d50642124bafd937db3b02f052bc504
eclipse021 <yahang2008@naver.com>
2025-12-01 04:38:35 +0900

    feat: 홈 화면에 appBar 적용

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:ssogssog_flutter/features/home/presentation/widget/home_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('홈 화면'),
+    return const Scaffold(
+      appBar: HomeAppBar(),
+      body: SingleChildScrollView(
+        // SingleChildScrollView를 사용해 화면이 길어져도 스크롤 가능하게 함
+        child: Column(
+          children: [
+            // 2. 메인 배너 (곧 만들 예정)
+            // 3. 액션 블록 (곧 만들 예정)
+            // 4. 내가 찜한 주식 배너 (곧 만들 예정)
+            // 5. 팩트 체크 섹션 (곧 만들 예정)
+          ],
+        ),
+      ),
     );
   }
 }
