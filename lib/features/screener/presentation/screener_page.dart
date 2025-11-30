a344f8d2929a6469fb7a509aab16e8ca93037642
eclipse021 <yahang2008@naver.com>
2025-12-01 01:20:45 +0900

    feat: 하단 탭(홈/조건검색/보관함/설정) 페이지 생성

---
+++ lib/features/screener/presentation/screener_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScreenerPage extends StatelessWidget {
+  const ScreenerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('조건검색 화면'),
+    );
+  }
+}
