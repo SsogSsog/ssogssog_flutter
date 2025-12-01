3361b5abc0980a1d16dc5f4c6ccaaea3679ed9c4
eclipse021 <yahang2008@naver.com>
2025-12-01 20:39:34 +0900

    feat: homePage 디자인 완성

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ssogssog_flutter/features/home/presentation/widget/action_blocks_section.dart';
+import 'package:ssogssog_flutter/features/home/presentation/widget/market_rank_section.dart';
 import 'package:ssogssog_flutter/features/home/presentation/widget/home_app_bar.dart';
 import 'package:ssogssog_flutter/features/home/presentation/widget/main_banner.dart';
+import 'package:ssogssog_flutter/features/home/presentation/widget/my_picks_banner.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,10 +15,10 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            MainBanner(), // 메인 배너 추가
-            // 3. 액션 블록 (곧 만들 예정)
-            // 4. 내가 찜한 주식 배너 (곧 만들 예정)
-            // 5. 팩트 체크 섹션 (곧 만들 예정)
+            MainBanner(),
+            ActionBlocksSection(),
+            MyPicksBanner(),
+            MarketRankingSection(),
           ],
         ),
       ),
