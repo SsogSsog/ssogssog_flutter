f855e6600ef95fe577b80198dffd1b1a67114684
eclipse021 <yahang2008@naver.com>
2025-12-01 20:34:01 +0900

    feat: Pretendard 폰트 도입 및 기본 폰트로 적용

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // 앱에서 사용할 색상을 정의하는 클래스
 class AppColors {
-  static const Color primaryBlue = Color(0xFF3F8CFF);       // 메인 파란색
+  static const Color primaryBlue = Color(0xFF55A4ED);       // 메인 파란색
   static const Color lightBlueBackground = Color(0xFFE9EFFB); // 연한 하늘색 배경
   static const Color white = Colors.white;                   // 흰색
   static const Color black = Colors.black;                   // 검은색
@@ -13,6 +13,8 @@ class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
       useMaterial3: true,
+
+      fontFamily: 'Pretendard',
       
       // 앱의 기본 색상 팔레트 정의
       colorScheme: const ColorScheme(
