d24bb67ee8b158c194d5440bc98e3ff865531dfb
eclipse021 <yahang2008@naver.com>
2025-12-28 19:46:24 +0900

    fix: 당기 순이익 그래프 색상 변경

--- lib/features/stock_detail/presentation/widget/performance_chart_card.dart
+++ lib/features/stock_detail/presentation/widget/performance_chart_card.dart
@@ -110,7 +110,7 @@ class _PerformanceChartCardState extends State<PerformanceChartCard> {
               data: data,
               valueSelector: (d) => d.netIncome,
               chartType: _ChartType.bar,
-              color: const Color(0xFFF2A23B), // 톤다운 오렌지
+              color: const Color(0xFF8098EA), // 톤다운 오렌지
             ),
           ),
         ],
