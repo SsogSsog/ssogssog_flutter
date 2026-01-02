6efc8b5947b5cb65110f51d6c104222000991235
eclipse021 <yahang2008@naver.com>
2026-01-02 18:43:21 +0900

    feat: `테마별 보기` 화면으로 이동을 위한 전역 route 설정

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -6,6 +6,7 @@ import 'package:ssogssog_flutter/features/screener/presentation/screener_page.da
 import 'package:ssogssog_flutter/features/screener/presentation/screener_result_page.dart';
 import 'package:ssogssog_flutter/features/settings/presentation/settings_page.dart';
 import 'package:ssogssog_flutter/features/stock_detail/presentation/stock_detail_page.dart';
+import 'package:ssogssog_flutter/features/themes/presentation/themes_page.dart'; // [추가]
 import 'package:ssogssog_flutter/features/vault/presentation/vault_page.dart';
 
 final GoRouter router = GoRouter(
@@ -46,14 +47,17 @@ final GoRouter router = GoRouter(
       path: '/screener/result',
       builder: (context, state) => const ScreenerResultPage(),
     ),
-    // 종목 상세 페이지를 위한 동적 라우트
     GoRoute(
-      path: '/stock/:stockCode', // ':'는 이 부분이 변수임을 의미
+      path: '/stock/:stockCode',
       builder: (context, state) {
-        // URL에서 stockCode 값을 추출
         final String stockCode = state.pathParameters['stockCode']!;
         return StockDetailPage(stockCode: stockCode);
       },
+    ),
+    // 테마 목록 페이지 라우트
+    GoRoute(
+      path: '/themes',
+      builder: (context, state) => const ThemesPage(),
     ),
   ],
 );
